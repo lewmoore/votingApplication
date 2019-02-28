8fb01e842a25d96136ba43ad572f2a3be581852a
Lewis Moore <lewis.moore2707@gmail.com>
2019-02-28 09:53:20 +0000

    Add poll route with data fetched from index.html

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/poll', (req, res) => {
+    fs.readFile(__dirname + '/poll.json', 'utf8', (err, data) => {
+        res.send(data)
+    })
 })
 
 app.listen(port, () => {
