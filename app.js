e85f29ff3f6cdc445594fa92e9dbb8f979bbfeff
Lewis Moore <lewis.moore2707@gmail.com>
2019-02-28 09:42:48 +0000

    Update index.html and add get route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
 app.listen(port, () => {
     console.log(`You're on port ` + port)
 })
